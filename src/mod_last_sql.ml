84b9e532dda9f978fae7f668da605e579bf31a22
Alexey Shchepin <alexey@process-one.net>
2011-07-06 17:29:32 +0300

    Removed accidentally added piece of code

--- src/mod_last_sql.ml
+++ src/mod_last_sql.ml
@@ -42,39 +42,3 @@ struct
 end
 
 let () = Gen_mod.register_mod (module ModLastSQL : Gen_mod.Module)
-
-let rec get_data_rec luser lserver els' res =
-    match els' with
-      | [] ->
-	  Lwt.return (List.rev res)
-      | (`XmlElement (_name, attrs, _)) as el :: els -> (
-	  let lxmlns = Xml.get_attr_s "xmlns" attrs in
-	  let username = (luser : Jlib.nodepreped :> string) in
-	  let query =
-	    <:sql<
-	      select @(data)s from private_storage
-              where username=%(username)s and
-              namespace=%(lxmlns)s
-	    >>
-	  in
-	  lwt private_data = Sql.query lserver query in (
-	      match private_data with
-		| [data] -> (
-		    try
-		      let newel = Xml.parse_element data in
-			get_data_rec luser lserver els (newel :: res)
-		    with
-		      | _ ->
-			  get_data_rec luser lserver els res
-		  )
-		| _ ->
-		    (* MREMOND: I wonder when the query could return a vcard ?1
-		       {selected, ["vcard"], []} ->
-		       EKHRAMTSOV: WTF??? *)
-		    get_data_rec luser lserver els (el :: res)
-	    ))
-      | _ :: els ->
-	  get_data_rec luser lserver els res
-
-  let get_data luser lserver els =
-    get_data_rec luser lserver els []
