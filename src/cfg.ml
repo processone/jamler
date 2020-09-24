4cf77996cf474d2ef5f2c0326a7790eacf989436
Alexey Shchepin <alexey@process-one.net>
2020-09-24 18:50:14 +0300

    Remove setup.ml and add missed files

---
+++ src/cfg.ml
@@ -0,0 +1,10 @@
+let os = Sys.os_type
+let version = "0.1"
+let system = "linux"
+let arch = "amd64"
+let name = "jamler"
+let ocaml = Sys.ocaml_version
+let synopsis = "OCaml Jabber Server"
+let copyright = "Copyright (c) 2002-2011 ProcessOne"
+let uri = "http://www.process-one.net/en/jamler/"
+let bday = "2011-05-30"
